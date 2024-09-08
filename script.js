b5128e2668b557e8229811627ad771eeadb3ba16
darshan-b-04 <152251778+darshan-b-04@users.noreply.github.com>
2024-09-08 11:01:34 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Back to top
+var amountScrolled = 200;
+var amountScrolledNav = 25;
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > amountScrolled) {
+        $('button.back-to-top').addClass('show');
+    } else {
+        $('button.back-to-top').removeClass('show');
+    }
+});
+
+$('button.back-to-top').click(function () {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 800);
+    return false;
+});
